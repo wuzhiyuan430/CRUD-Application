0383573add7a7e3b7b95e08da04eadd0f6c4efcf
wuzhiyuan430 <wuzhiyuan430@gmail.com>
2017-07-24 20:24:07 -0400

    I find my confidence again.

--- tests/test_products_app.py
+++ tests/test_products_app.py
@@ -3,7 +3,7 @@ def enlarge(i):
     return i * 100
 
 # this is the my_test.py
-from app.products_app import * # load accompanying code (i.e. the `enlarge()` function to avoid NameError: name 'enlarge' is not defined
+#from app.products_app import * # load accompanying code (i.e. the `enlarge()` function to avoid NameError: name 'enlarge' is not defined
 
 def test_enlarge(): # name this function anything, but hopefully something corresponding to the function it is testing
     result = enlarge(3)
